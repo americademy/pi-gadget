c2baaf2fd9c01dd4d01b0e029fff7f0a42c56d31
Craig Ulliott <craigulliott@gmail.com>
2019-12-07 15:42:52 -0600

    using master branch

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -10,10 +10,10 @@ echo "Configure git for Buildkite"
 git config --global user.name machine-americademy
 git config --global user.email engineering@americademy.com
 
-echo "Pulling latest from 18-armhf branch"
-git reset --hard origin/18-armhf
-git checkout 18-armhf
-git pull origin 18-armhf
+echo "Pulling latest from master branch"
+git reset --hard origin/master
+git checkout master
+git pull origin master
 
 echo "Grabbing release info"
 RELEASE_NOTES=$(buildkite-agent meta-data get "release-notes")
@@ -53,5 +53,5 @@ git commit -m "Updated build v$VERSION" -m "Release v$VERSION" -m "$RELEASE_NOTE
 # the [skip ci] will prevent another master build based on this commit
 git tag -a $VERSION -m "Release v$VERSION"
 
-echo "Pushing new version to 18-armhf"
-git push origin 18-armhf && git push --tags
+echo "Pushing new version to master"
+git push origin master && git push --tags
