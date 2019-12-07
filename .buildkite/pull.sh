811a551daf26a113d9061407f0019fd4b9cade51
Craig Ulliott <craigulliott@gmail.com>
2019-12-07 15:29:00 -0600

    Update pull.sh

--- .buildkite/pull.sh
+++ .buildkite/pull.sh
@@ -3,7 +3,7 @@
 set -eu
 
 echo "Pulling latest master"
-git reset --hard origin/master
-git checkout master
-git pull origin master
+git reset --hard origin/18-armhf
+git checkout 18-armhf
+git pull origin 18-armhf
 
